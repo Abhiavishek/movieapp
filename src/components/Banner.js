76ccbce00754d55679f6521feafa03444dcd5e17
Abhiavishek <avi74500@gmail.com>
2023-05-21 21:19:21 +0530

    MovieWeBApp

---
+++ src/components/Banner.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import { movies } from "../moviesData";
+
+export class Banner extends Component {
+  render() {
+    let movieElem = movies.results[Math.floor(Math.random() * 10)];
+    let backDrop = movieElem.backdrop_path;
+    let title = movieElem.title
+    let description = movieElem.overview;
+    return (
+      <div className="card banner-card">
+        <img
+          src={`https://image.tmdb.org/t/p/original${backDrop}`}
+          className="card-img-top banner-img"
+          alt="..."
+        />
+
+        <h5 className="card-title banner-title">{title}</h5>
+        <p className="card-text banner-text">
+         {description}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Banner;
